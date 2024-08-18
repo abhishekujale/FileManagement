891e5718b18c7871b07e68425e1eda52aa97fb07
abhishekujale <abhishekujale.kitcse18@gmail.com>
2024-08-18 16:18:29 +0530

    Having Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ToastContainer } from "react-toastify";
 
 import Register from "./components/authentication/Register";
 import Login from "./components/authentication/Login";
-import NavbarComponent from "./components/Navbar";
+import NavbarComponent from "./components/Navbar";  
 import Dashboard from "./components/Dashboard";
 
 import "./App.css";
